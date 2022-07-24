aaee9a591f777a0495c3a0988986dfd59fdd5d57
romantanzerfamily <roman@tanzer.family>
2022-07-24 21:01:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-qfxf1y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-qfxf1y)
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+
+setupCounter(document.querySelector('#counter'))
